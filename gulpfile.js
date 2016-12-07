f1a19028da5807f7f8efbc5b9a6567f481274744
Stephanie Zeng <stephaniezeng521@gmail.com>
2016-12-07 02:00:45 -0500

    Added gulpfile.js and installed postcss.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+
+gulp.task('html', function(){
+  // do something here
+});
+
+gulp.task('styles',function(){
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([cssvars, nested, autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles/'));
+});
+
+gulp.task('watch', function(){
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  }); // watch html
+  watch('./app/assets/**/*.css', function(){
+    gulp.start('styles');
+  }); // watch styles
+});
