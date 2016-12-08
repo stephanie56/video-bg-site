9a189c91f69c19a569ffc288e0b367a8317e7690
Stephanie Zeng <stephaniezeng521@gmail.com>
2016-12-07 23:07:52 -0500

    Added postcss import

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 gulp.task('html', function(){
   // do something here
